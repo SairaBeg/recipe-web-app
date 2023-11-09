989bf59e0219d1ade5ac5b8a939e055871076f52
SairaBeg <Sairabeg88@gmail.com>
2023-11-08 22:16:41 -0500

    Login

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -2,6 +2,10 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { UserModel } from "../models/Users.js";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: "./.env" });
 
 const router = express.Router();
 
@@ -18,6 +22,20 @@ router.post("/register", async (req, res) => {
   res.json({ message: "User Registered Successfully!" });
 });
 
-router.post("/login");
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username });
+
+  if (!user) {
+    return res.json({ message: "User does not exist!" });
+  }
+  const isPwValid = await bcrypt.compare(password, user.password);
+  if (!isPwValid) {
+    return res.json({ message: "Username or Password is incorrect." });
+  }
+  console.log(process.env.JWT_SECRET);
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res.json({ token, userID: user._id });
+});
 
 export { router as userRouter };
