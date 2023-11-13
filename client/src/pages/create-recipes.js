deef7476f98e58ef5283177f01385a8615c15e40
SairaBeg <Sairabeg88@gmail.com>
2023-11-12 19:02:43 -0500

    Ingredients add input fields

--- client/src/pages/create-recipes.js
+++ client/src/pages/create-recipes.js
@@ -1,14 +1,46 @@
+import { useState } from "react";
+
 export const CreateRecipe = () => {
+  const [recipe, setRecipe] = useState({
+    name: "",
+    ingredients: [],
+    instructions: "",
+    imageUrl: "",
+    cookingTime: 0,
+    userOwner: 0,
+  });
+  const handleChange = (event, idx) => {
+    const { value } = event.target;
+    const ingredients = recipe.ingredients;
+    ingredients[idx] = value;
+    setRecipe({ ...recipe, ingredients });
+  };
+  const addIngredient = () => {
+    setRecipe({ ...recipe, ingredients: [...recipe.ingredients, ""] });
+  };
+  const handleIngredientChange = (event) => {
+    const { name, value } = event.target;
+    setRecipe({ ...recipe, [name]: value });
+  };
   return (
     <div className="create-recipe">
       <h2>Create Recipe</h2>
       <form>
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
+        <input type="text" name="name" id="name" onChange={handleChange} />
         <label htmlFor="description">Description</label>
         <input type="text" name="description" id="description" />
         <label htmlFor="ingredients">Ingredients</label>
-        <input type="text" name="ingredients" id="ingredients" />
+        {recipe.ingredients.map((ingredient, idx) => (
+          <input
+            key={idx}
+            type="text"
+            name="ingredients"
+            value={ingredient}
+            onChange={(event) => handleIngredientChange(event, idx)}
+          />
+        ))}
+        <button onClick={addIngredient}>Add Ingredient</button>
 
         <label htmlFor="instructions">Instructions</label>
         <textarea type="text" name="instructions" id="instructions" />
