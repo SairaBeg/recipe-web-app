7580f0c2dfc9f0911a1c1456a3b6df100b4f1265
SairaBeg <Sairabeg88@gmail.com>
2023-11-13 22:22:47 -0500

    New recipes sending to database

--- client/src/pages/create-recipes.js
+++ client/src/pages/create-recipes.js
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import axios from "axios";
+import { useGetUserID } from "../hooks/useGetUserID";
 
 export const CreateRecipe = () => {
+  const userID = useGetUserID();
   const [recipe, setRecipe] = useState({
     name: "",
     ingredients: [],
     instructions: "",
     imageUrl: "",
     cookingTime: 0,
-    userOwner: 0,
+    userOwner: userID,
   });
 
   const handleChange = (event) => {
@@ -27,9 +29,15 @@ export const CreateRecipe = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axios.post();
+      const response = await axios.post(
+        "http://localhost:3001/recipes",
+        recipe
+      );
+      alert("Recipe Created!");
+      console.log(response);
     } catch (e) {
-      console.error(e);
+      console.error("Error submitting recipe:", e);
+      alert("Error creating recipe. Please check the console for details.");
     }
   };
   return (
@@ -37,17 +45,29 @@ export const CreateRecipe = () => {
       <h2>Create Recipe</h2>
       <form onSubmit={onSubmit}>
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" onChange={handleChange} />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={recipe.name}
+          onChange={handleChange}
+        />
         <label htmlFor="description">Description</label>
-        <input type="text" name="description" id="description" />
+        <textarea
+          value={recipe.description}
+          name="description"
+          id="description"
+          onChange={handleChange}
+        />
         <label htmlFor="ingredients">Ingredients</label>
-        {recipe.ingredients.map((ingredient, idx) => (
+        {recipe.ingredients.map((ingredient, index) => (
           <input
-            key={idx}
+            key={index}
             type="text"
             name="ingredients"
+            id="ingredients"
             value={ingredient}
-            onChange={(event) => handleIngredientChange(event, idx)}
+            onChange={(event) => handleIngredientChange(event, index)}
           />
         ))}
         <button type="button" onClick={addIngredient}>
@@ -55,11 +75,29 @@ export const CreateRecipe = () => {
         </button>
 
         <label htmlFor="instructions">Instructions</label>
-        <textarea type="text" name="instructions" id="instructions" />
+        <textarea
+          value={recipe.instructions}
+          onChange={handleChange}
+          type="text"
+          name="instructions"
+          id="instructions"
+        />
         <label htmlFor="imageUrl">ImageURL</label>
-        <input type="text" name="imageUrl" id="imageUrl" />
+        <input
+          value={recipe.imageUrl}
+          onChange={handleChange}
+          type="text"
+          name="imageUrl"
+          id="imageUrl"
+        />
         <label htmlFor="cookingTime">Cooking Time</label>
-        <input type="number" name="cookingTime" id="cookingTime" />
+        <input
+          value={recipe.cookingTime}
+          onChange={handleChange}
+          type="number"
+          name="cookingTime"
+          id="cookingTime"
+        />
         <button type="submit">Create Recipe</button>
       </form>
     </div>
