8871bb313c20eabc3f80392c0ea1366ca6dbc95c
SairaBeg <Sairabeg88@gmail.com>
2023-11-16 20:08:37 -0500

    Saved Recipe page created

--- client/src/pages/saved-recipes.js
+++ client/src/pages/saved-recipes.js
@@ -1,3 +1,45 @@
-export const SavedRecipes = () => {
-  return <div>SavedRecipes</div>;
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useGetUserID } from "../hooks/useGetUserID";
+
+export const setSavedRecipes = () => {
+  const [savedRecipes, setSavedRecipes] = useState([]);
+
+  const userID = useGetUserID();
+
+  useEffect(() => {
+    const fetchSavedRecipe = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/recipes/savedRecipes/${userID}`
+        );
+
+        setSavedRecipes(response.data.savedRecipes);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    fetchSavedRecipe();
+  }, []);
+
+  return (
+    <div>
+      <h2>Saved Recipes</h2>
+      <ul>
+        {savedRecipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h2>{recipe.name}</h2>
+            </div>
+            <div className="instructions">
+              <p>{recipe.instructions}</p>
+              <img src={recipe.imageUrl} alt={recipe.name} />
+              <p>Cooking Time: {recipe.cookingTime} (minutes)</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
